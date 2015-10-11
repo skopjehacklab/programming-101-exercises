1ce1a9a244e0304e3c441384d13516abed1c4a3b
Andrej T <andrejtrajchevski@gmail.com>
2015-10-11 16:32:50 +0200

    added pick-a-random-student script

---
+++ 0/pick-a-student.py
@@ -0,0 +1,10 @@
+import sys
+import random
+
+number_of_students = int(sys.argv[1])
+student_ids = list(range(number_of_students))
+random.shuffle(student_ids)
+
+for sid in student_ids:
+    print(sid)
+    input("paused.")
