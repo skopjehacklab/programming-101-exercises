cf218479c778bde03b3ce7d2eb864a6c29b3edd1
Andrej T <andrejtrajchevski@gmail.com>
2015-10-24 21:08:40 +0200

    login exercise

---
+++ 2/login.py
@@ -0,0 +1,34 @@
+# Building X has an electronic door mechanism. Only people
+# on the whitelist can enter the building.
+#
+# Your task is to write a function that checks if the
+# person trying to enter the building is on the whitelist.
+#
+# Complete the function: given a username and their password,
+# it checks if the user has supplied the correct password.
+#
+# Returns `True` or `False`, depending on whether they're
+# allowed to enter or not.
+#
+# Hint: the supplied username will always be correct.
+
+
+def enter(username, password):
+	whitelist = [
+		{ user: 'killy', 'age': 29, 'password': 'youshallnotpass' },
+		{ user: 'hacker', 'age': 17, 'password': 't00str0ng1' },
+		{ user: 'whoeverest', 'age': 25, 'pasword': 'blahbleh' }
+	]
+
+    pass # delete "pass" and write your code here
+
+
+# Tests (do NOT edit!)
+
+res1 = enter('killy', 'youshallnotpass')
+res2 = enter('whoeverest', 'youshallnotpass')
+res3 = enter('hacker', '12312321')
+
+assert(res1 == True)
+assert(res2 == False)
+assert(res3 == False)
